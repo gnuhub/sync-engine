47326b8ab4b075eb4fc97d03f2eb91d5ac3d6ab7
stallman <gnuhub@icloud.com>
2018-06-14 15:31:06 +0800

    fix #3 atal error: 'my_config.h' file not found

--- mac.setup.sh
+++ mac.setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# https://github.com/gnuhub/sync-engine/issues/2
+brew install lua
+# https://github.com/gnuhub/sync-engine/issues/3
+brew install mysql-connector-c
+
 source activate py27
 conda env export -n py27 > py27.yaml
 pip install -r requirements.txt
