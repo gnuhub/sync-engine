2d8198165187f0ceb1a8dd01d8a284813cb23cda
stallman <gnuhub@icloud.com>
2018-06-14 17:09:46 +0800

    #9 Can't connect to local MySQL server through socket '/tmp/mysql.sock'

--- mac.setup.sh
+++ mac.setup.sh
@@ -6,6 +6,9 @@ brew install mysql-connector-c
 # https://github.com/gnuhub/sync-engine/issues/8
 sudo ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
+# https://github.com/gnuhub/sync-engine/issues/9#issuecomment-397225469
+ln -s /Applications/XAMPP/xamppfiles/var/mysql/mysql.sock /tmp/mysql.sock
+
 source activate py27
 pip uninstall -y python-dateutil
 pip uninstall -y flanker
