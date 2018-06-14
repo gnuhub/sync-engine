1a7205f2ae192a6d33b9a90ce5a13f3be14caeb7
stallman <gnuhub@icloud.com>
2018-06-14 10:48:01 +0000

    update mac.setup.sh

--- mac.setup.sh
+++ mac.setup.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
+
 # https://github.com/gnuhub/sync-engine/issues/2
 brew install lua
+
 # https://github.com/gnuhub/sync-engine/issues/3
 brew install mysql-connector-c
+
 # https://github.com/gnuhub/sync-engine/issues/8
+# some more actions
+# https://github.com/gnuhub/sync-engine/issues/8#issuecomment-397231081
 sudo ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 # https://github.com/gnuhub/sync-engine/issues/9#issuecomment-397225469
-ln -s /Applications/XAMPP/xamppfiles/var/mysql/mysql.sock /tmp/mysql.sock
+if [ -d /Applications/XAMPP ];then
+    ln -sv /Applications/XAMPP/xamppfiles/var/mysql/mysql.sock /tmp/mysql.sock
+fi
 
+# https://github.com/gnuhub/sync-engine/issues/12
 sudo mkdir -p /var/lib/inboxapp/parts
 sudo chmod -R 777 /var/lib/inboxapp
 
