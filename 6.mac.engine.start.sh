efa46fa056fb9b0ae6ca9c11a64befb5a0d20f85
stallman <gnuhub@icloud.com>
2018-07-17 05:43:58 +0000

    fix libxml2

--- 6.mac.engine.start.sh
+++ 6.mac.engine.start.sh
@@ -2,6 +2,7 @@
 set -x
 echo "run bin/ctreate-db  bin/ctreate-test-db"
 
+export DYLD_FALLBACK_LIBRARY_PATH=/usr/lib:/usr/local/lib:${HOME}/anaconda/lib/:/Volumes/data/anaconda/lib/:$DYLD_FALLBACK_LIBRARY_PATH
 export PATH=${HOME}/anaconda/bin/:$PATH
 
 CMD_PATH=$(cd `dirname $0`; pwd)
