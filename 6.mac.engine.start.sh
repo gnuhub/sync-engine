21881ed747faac33b28a4dae3745da6c519f3a79
leftree <leftreefile@leftreedeMacBook-Air.local>
2018-07-18 09:43:13 +0000

    add jsc_api.py

--- 6.mac.engine.start.sh
+++ 6.mac.engine.start.sh
@@ -2,6 +2,8 @@
 set -x
 echo "run bin/ctreate-db  bin/ctreate-test-db"
 
+export DYLD_FALLBACK_LIBRARY_PATH=/usr/lib:/usr/local/lib:/Volumes/data/anaconda/lib/:$DYLD_FALLBACK_LIBRARY_PATH
+
 export PATH=${HOME}/anaconda/bin/:$PATH
 
 CMD_PATH=$(cd `dirname $0`; pwd)
