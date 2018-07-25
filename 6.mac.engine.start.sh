66d3cf5e7798ca2a9cff8c3f234a1b4ad2028e96
Liu Yang <yang6.liu@gmail.com>
2018-07-25 00:51:17 +0000

    fix libxml2

---
+++ 6.mac.engine.start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x
+echo "run bin/ctreate-db  bin/ctreate-test-db"
+
+export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/Cellar/libxml2/2.9.7/lib:/usr/lib:/usr/local/lib:${HOME}/anaconda/lib/:/Volumes/data/anaconda/lib/:$DYLD_FALLBACK_LIBRARY_PATH
+export PATH=${HOME}/anaconda/bin/:$PATH
+
+CMD_PATH=$(cd `dirname $0`; pwd)
+source activate py27
+
+export PYTHONPATH=$CMD_PATH
+export NYLAS_ENV=dev
+
+./bin/inbox-start
