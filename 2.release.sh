bf7e0b18df3460e31b6a1e06e08f384555660290
stallman <gnuhub@icloud.com>
2018-07-26 03:21:00 +0000

    fix git not found

--- 2.release.sh
+++ 2.release.sh
@@ -1,5 +1,4 @@
 set -x
 ## 获取当前源码版本
-VERSION=$(git rev-parse --short HEAD)
 docker build -t registry.gitlab.com/bxqd_gitlabci_201807/sync-engine:latest .
 docker push registry.gitlab.com/bxqd_gitlabci_201807/sync-engine:latest
