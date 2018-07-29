f94e67c5d4209398ae8520b0aef305ad135550d4
stallman <gnuhub@icloud.com>
2018-07-29 22:55:52 +0000

    Y18-3 fix docker.sync-engine-init.sh to create-db

--- docker.sync-engine-init.sh
+++ docker.sync-engine-init.sh
@@ -1,9 +1,9 @@
 #usr/bin/env bash
-
+source activate py27
 ## 记录当前路径
 CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 # after mysql started
 sleep 10
 PYTHONPATH=`pwd` NYLAS_ENV=dev bin/create-db
-PYTHONPATH=`pwd` NYLAS_ENV=dev bin/create-test-db
+#PYTHONPATH=`pwd` NYLAS_ENV=dev bin/create-test-db
