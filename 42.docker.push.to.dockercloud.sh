5c4aed2a6510404baa0897baa0f9124443f2e7b9
stallman <gnuhub@icloud.com>
2018-07-29 23:01:19 +0000

    Y18-3 tag gnuhub/sync-engine:latest

--- 42.docker.push.to.dockercloud.sh
+++ 42.docker.push.to.dockercloud.sh
@@ -2,4 +2,6 @@
 VERSION=$(git rev-parse --short HEAD)
 
 docker tag sync-engine:$VERSION gnuhub/sync-engine:$VERSION
+docker tag sync-engine:$VERSION gnuhub/sync-engine:latest
 docker push gnuhub/sync-engine:$VERSION
+docker push gnuhub/sync-engine:latest
