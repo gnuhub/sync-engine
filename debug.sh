98680ad18923ae1be9291c20900c164c8fa36aad
stallman <gnuhub@icloud.com>
2018-07-29 03:14:14 +0000

    Y18-3 install c tools

---
+++ debug.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run  -v ${PWD}:/data daocloud.io/gnuhub3/sync-engine_docker:daocloud-79d6b26 bash /data/docker.sh
--- docker.sh
+++ docker.sh
@@ -8,10 +8,34 @@ apt-get clean -y
 apt-get update -y
 apt-get install -f -y
 apt-get install lsb-core -y
+if [ ! -f mysql-apt-config_0.8.9-1_all.deb ];then
 wget https://repo.mysql.com/mysql-apt-config_0.8.9-1_all.deb
-
+fi
 dpkg -i mysql-apt-config_0.8.9-1_all.deb
 apt-get update -y
+
+apt-get install -y build-essential
+apt-get install -y m4
+apt-get install -y flex
+apt-get install -y bison
+apt-get install -y gawk
+apt-get install -y texinfo
+apt-get install -y autoconf
+apt-get install -y libtool
+apt-get install -y pkg-config
+apt-get install -y openssl 
+apt-get install -y curl 
+apt-get install -y libreadline6 
+apt-get install -y git 
+apt-get install -y zlib1g 
+apt-get install -y autoconf 
+apt-get install -y automake 
+apt-get install -y libtool 
+apt-get install -y imagemagick 
+apt-get install -y make
+apt-get install -y tree
+apt-get install -y gdb
+apt-get install lua -y
 apt-get install default-libmysqlclient-dev -y
 apt-get install mysql-client -y
 
