15f28a5ff6328122d022e3d0ebd26d8ab21fc3cb
stallman <gnuhub@icloud.com>
2018-07-30 00:02:57 +0000

    Y18-3 fix Don't run the Nylas Sync Engine as root

--- docker.start.engine.sh
+++ docker.start.engine.sh
@@ -5,5 +5,5 @@ CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 # after mysql started
 sleep 2
-su - syncengine -c "PYTHONPATH=`pwd` NYLAS_ENV=dev bin/create-db"
-su - syncengine -c "PYTHONPATH=`pwd` NYLAS_ENV=dev ./bin/inbox-start"
+PYTHONPATH=`pwd` NYLAS_ENV=dev bin/create-db
+PYTHONPATH=`pwd` NYLAS_ENV=dev ./bin/inbox-start
