7f8c64feb1609f4c0697c0a6fd650df18fb1b829
stallman <gnuhub@icloud.com>
2018-09-05 18:42:52 +0800

    fix user

--- docker.start.api.sh
+++ docker.start.api.sh
@@ -3,6 +3,6 @@ set -x
 source activate py27
 ## 记录当前路径
 # CMD_PATH=$(cd `dirname $0`; pwd)
-cd /home/syncengine3/
+cd /home/syncengine4/
 sleep 40
 PYTHONPATH=`pwd` NYLAS_ENV=dev bin/inbox-api -p 9999
--- docker.start.engine.sh
+++ docker.start.engine.sh
@@ -3,7 +3,7 @@ set -x
 source activate py27
 ## 记录当前路径
 # CMD_PATH=$(cd `dirname $0`; pwd)
-cd /home/syncengine3/
+cd /home/syncengine4/
 # after mysql started
 sleep 30
 PYTHONPATH=`pwd` NYLAS_ENV=dev bin/create-db
